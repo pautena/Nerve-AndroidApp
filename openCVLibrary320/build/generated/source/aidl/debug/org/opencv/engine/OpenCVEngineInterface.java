665dde8f6b2be41555d24f1e7fcd3b6bf5261b2f
Albert Segarra Roca <albertsgrc@gmail.com>
2017-03-05 06:54:37 +0100

    Update gradle

--- openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
+++ openCVLibrary320/build/generated/source/aidl/debug/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: /Users/pautenavidal/AndroidStudioProjects/HackUPC/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
+ * Original file: /Users/albert/nerve/HackUPCAndorid/openCVLibrary320/src/main/aidl/org/opencv/engine/OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
